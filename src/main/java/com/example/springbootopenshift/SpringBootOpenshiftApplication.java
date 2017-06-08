b6630f09cea3332111aa0b27ed8a5885b9b9110a
Marcin Zajączkowski <marcin.zajaczkowski@4finance.com>
2017-06-08 13:03:33 +0200

    Add / mapping

--- src/main/java/com/example/springbootopenshift/SpringBootOpenshiftApplication.java
+++ src/main/java/com/example/springbootopenshift/SpringBootOpenshiftApplication.java
@@ -2,9 +2,16 @@ package com.example.springbootopenshift;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.web.bind.annotation.RestController;
 
 @SpringBootApplication
+@RestController
 public class SpringBootOpenshiftApplication {
+
+	@RequestMapping("/")
+	public String home() {
+		return "Hello Openshift World";
+        }
 
 	public static void main(String[] args) {
 		SpringApplication.run(SpringBootOpenshiftApplication.class, args);

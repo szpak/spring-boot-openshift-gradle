7e6aa8d97517ae2dafb902c0d353cb3a6a27e7e7
Marcin Zajączkowski <marcin.zajaczkowski@4finance.com>
2017-06-08 12:12:59 +0200

    Initial import - Gradle edition
    OpenShift seems to ignore provided reference.

---
+++ src/main/java/com/example/springbootopenshift/SpringBootOpenshiftApplication.java
@@ -0,0 +1,12 @@
+package com.example.springbootopenshift;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringBootOpenshiftApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringBootOpenshiftApplication.class, args);
+	}
+}
